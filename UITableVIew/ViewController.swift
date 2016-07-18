a9181aea3c890017a5b96d6568320dbdc8f0bddc
Ainor Syahrizal <ainorsyahrizal@ainors-macbook-pro.local>
2016-07-18 20:51:23 +0800

    create number of rows in section and display string

--- UITableVIew/ViewController.swift
+++ UITableVIew/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,3 +26,26 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    // a UITableViewDataSource must have
+    // numberOfRowsInSection
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    // and cellForRowAtIndex
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        if indexPath.row == 0 {
+            cell.textLabel?.text = "Row Zero"
+        } else if indexPath.row == 1 {
+            cell.textLabel?.text = "Row One"
+        } else if indexPath.row == 2 {
+            cell.textLabel?.text = "Row Two"
+        }
+        return cell
+    }
+}
